d3275500b4b27069495f45b092121b459145e974
tmfhirsch <tmfhirsch@users.noreply.github.com>
2020-03-26 14:39:55 +1100

    Fixed indents, allowed density or step size inputs

--- DESketch.jl
+++ DESketch.jl
@@ -11,10 +11,10 @@ expAnalytic=(x->exp(-5-5*x))
 expDensity,expLeft,expRight=100,-1,10
 
 function expDecayExample()
-grid,res,err=nDEulerSolver(expDensity,expLeft,expRight,expMatrix,expInitial,expAnalytic)
-print("Error = $err\n")
-plot(grid,res)
-plot!(grid,expAnalytic.(grid))
+    grid,res,err=nDEulerSolver('d',expDensity,expLeft,expRight,expMatrix,expInitial,expAnalytic)
+    print("Error = $err\n")
+    plot(grid,res)
+    plot!(grid,expAnalytic.(grid))
 end
 
 # Conservation of population example
@@ -25,11 +25,31 @@ consAnalytic=(x->-sin(x)+2*cos(x))
 consDensity,consLeft,consRight=100,0,2*π
 
 function consDecayExample()
-grid,res,err=nDEulerSolver(consDensity,consLeft,consRight,consMatrix,consInitial,consAnalytic)
-print("Error = $err\n")
-plot(grid,res)
-plot!(grid,consAnalytic.(grid))
+    grid,res,err=nDEulerSolver('d',consDensity,consLeft,consRight,consMatrix,consInitial,consAnalytic)
+    print("Error = $err\n")
+    plot(grid,res)
+    plot!(grid,consAnalytic.(grid))
 end
 
-function iterator(key::string,lEnd,rEnd,lΔ,uΔ)
-    
+function iterator(key::String,lΔ,uΔ,nΔ)
+    ΔGrid=range(lΔ,uΔ,length=nΔ)
+    @assert key in ["exp","cons"] "Invalid key: $key"
+    errors=[]
+    if key == "exp" #Exponential example
+        for Δ in ΔGrid
+            grid,res,err=nDEulerSolver('s',Δ,expLeft,expRight,expMatrix,expInitial,expAnalytic)
+            push!(errors,err)
+        end
+        plot(ΔGrid,errors,xaxis=:log,yaxis=:log,
+        xlabel="Step size", ylabel="Average error",
+        title="$key n-dim Euler's Method solver")
+    else #Conservation of population/Wave equation example
+        for Δ in ΔGrid
+            grid,res,err=nDEulerSolver('s',Δ,consLeft,consRight,consMatrix,consInitial,consAnalytic)
+            push!(errors,err)
+        end
+        plot(ΔGrid,errors,xaxis=:log,yaxis=:log,
+        xlabel="Step size", ylabel="Average error",
+        title="$key n-dim Euler's Method solver, n=$nΔ")
+    end
+end
