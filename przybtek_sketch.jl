d01b6f6961435633642c236aa219aab731bbf395
tmfhirsch <tmfhirsch@users.noreply.github.com>
2020-07-15 21:11:02 +1000

    got przybytek_sketch working with units

--- przybtek_sketch.jl
+++ przybtek_sketch.jl
@@ -15,18 +15,31 @@ using Revise
 All inputs in units, or assumed to be in atomic units if not.
 Inputs: energy, limits; V(R)=przybytek, m=He₂ mass
 Output: (ψ,ψ') on RHS of limits"""
-#=function rhs_solver(ϵ;
-                    limits=[10.,11.]u"bohr",
-                    V=przybytek,
-                    m=2*4.002602u"u")
+function rhs_solver(ϵ;
+                    stapt=20.0u"bohr", # location of IC
+                    endpt=500.0u"bohr", # RHS to be solved to
+                    V=przybytek, # interatomic potential
+                    m=2*4.002602u"u" # He₂ mass)
+                    )
     function TISE!(du,u,p,x) # TISE gives (ψ,ψ')'=f((ψ,ψ'))
-        du[1]=u[2] # ψ'(x)≡ψ'(x)
-        du[2]=2*m*(V(x)-ϵ)*u[1]/1u"ħ^2" # (ψ'(x))'=2m(V(x)-E)ψ(x)/ħ^2
+        du.w = u.d # ψ'(x)≡ψ'(x)
+        du.d = 2*m*(V(x)-ϵ)*(u.w)/1u"ħ^2" # (ψ'(x))'=2m(V(x)-E)ψ(x)/ħ^2
     end
-    IC=[0.0, 1.0u"bohr^-1"] # (ψ,ψ')=(0,1)
-    prob=ODEProblem(TISE!,IC,limits)
+    w0 = [0.0] #ψ₀ = 0
+    d0 = [1.0]u"bohr^-1" #ψ₀'= 1
+    IC=ComponentArray(w=w0,d=d0)
+    prob=ODEProblem(TISE!,IC,[stapt,endpt])
     sol=solve(prob,Tsit5())
-end=#
+    return sol
+end
+sol=rhs_solver(10u"hartree",stapt=1.0u"bohr",endpt=150.0u"bohr")
+Rs=sol.t
+ψs=[i[1] for i in sol.u[:]]
+ψplot=plot(ustrip.(Rs), ustrip.(ψs), legend=false)
+Vs=przybytek.(Rs)
+Vplot=plot(ustrip.(Rs), ustrip.(Vs), legend=false)
+plot(ψplot, Vplot, layout=(2,1))
+#=
 # Test of above
 ϵ=1u"hartree"
 limits=[10.,11.]u"bohr"
@@ -41,6 +54,7 @@ d0 = [1.0]u"bohr^-1" #ψ₀'= 1
 IC=ComponentArray(w=w0,d=d0)
 prob=ODEProblem(TISE!,IC,limits)
 sol=solve(prob,Tsit5())
+=#
 
 #=
 #Simple example demonstrating bug I find with second order ODEs
